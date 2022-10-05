7a0cc5843c1585ca16d34d5f7812903c87bfbecb
Anastasiya Kim <anastasiyapotard@gmail.com>
2022-10-05 13:27:59 +0200

    changed routes and added column

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "restaurants#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :create, :new, :show, :update] do
+  resources :restaurants, only: [:index, :create, :new, :destroy, :show, :update] do
     resources :reviews, only: [:index, :create, :destroy]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_180155) do
+ActiveRecord::Schema.define(version: 2022_10_05_112256) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2022_07_19_180155) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
